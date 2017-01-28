2bc30b000b0f3fee4ced98f953f0e8966a358fc1
Brian Johnston <b_johnston98@yahoo.com>
2017-01-28 18:41:38 -0500

    blank arrtest removed

--- MM_Kata_redo.rb
+++ MM_Kata_redo.rb
@@ -5,4 +5,8 @@ def mined_minds_kata_array #the array to be made
 	# (1..100).to_a	#easierst way to make it pass for now, array.method
 		kata_array = []	# well push all results here, the 100 elemts.
 
+	number = 1
+	kata_array << number #push 1 into kata_array
 end
+
+
--- TestMM_Kata_redo.rb
+++ TestMM_Kata_redo.rb
@@ -3,12 +3,13 @@ require_relative "MM_Kata_redo.rb"
 
 class TestMM_Kata_redo_Functions < Minitest::Test
 
-	def test_find_blank_array 	#can be anything
-		result = mined_minds_kata_array # this name Must be used exact in code
+	#def test_find_blank_array 	#can be anything
+	#	result = mined_minds_kata_array # this name Must be used exact in code
 										# points to codefunction 2b tested
-		assert_equal([], mined_minds_kata_array)	#since result = MMKA, could just put MMKA in here.
+	#	assert_equal([], mined_minds_kata_array)	#since result = MMKA, could just put MMKA in here.
 									 
-	end
+	#end
+	#we started with blank array, but now pushing 1, so blanktest fails.
 
 	def test_1_returns_1
 		assert_equal(1, mined_minds_kata_array[0]) #indexed to 1st position in array
