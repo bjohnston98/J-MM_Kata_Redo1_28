bf4a1f1bac3d2c0f33c574c95e1bc617e13b6ae4
Brian Johnston <b_johnston98@yahoo.com>
2017-01-28 18:30:58 -0500

    one pass, one fail

---
+++ MM_Kata_redo.rb
@@ -0,0 +1,8 @@
+# this is the Code to be tested
+#we want an array of 100 elements
+
+def mined_minds_kata_array #the array to be made
+	# (1..100).to_a	#easierst way to make it pass for now, array.method
+		kata_array = []	# well push all results here, the 100 elemts.
+
+end
